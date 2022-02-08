22d2e004b126c5344c80389be7c87227309d0d60
Fergus <fmcnab93@gmail.com>
2022-02-08 17:05:34 +0100

    Add plot to PF16_S7 example.

--- run_PF16_S7.py
+++ run_PF16_S7.py
@@ -13,7 +13,7 @@ print("Our result:                       P = %.2f GPa, T = %i oC." %
 # ---- Set up mantle
 lz = m.lithologies.katz.lherzolite()
 mantle = m.Mantle([lz], [1], ['Lz'])
-P_sol = np.arange(0., 6., 0.1)
+P_sol = np.arange(0., 3., 0.1)
 T_sol = [lz.TSolidus(P) for P in P_sol]
 path = mantle.AdiabaticMelt_1D(1330., Pstart=6., steps=101)
 
@@ -25,11 +25,17 @@ print("Best-fitting melting model: Tp = %i oC, F = %.2f %%." %
     (s.individual_potential_temperatures['Tp'], 
     s.individual_potential_temperatures['F_path_ind']*100.))
     
+
 # ---- Plot
-plt.plot(T_sol, P_sol, "k")
-plt.plot(s.individual_potential_temperatures.iloc[0]['path'].T, s.individual_potential_temperatures.iloc[0]['path'].P, "--")
-plt.scatter(s.PT['T'], s.PT['P'], marker="*")
+melt_label = "Melting path, $T_p = %i" % s.individual_potential_temperatures.iloc[0]['Tp']
+plt.plot(T_sol, P_sol, "k", label="Solidus")
+plt.plot(
+    s.individual_potential_temperatures.iloc[0]['path'].T, 
+    s.individual_potential_temperatures.iloc[0]['path'].P, 
+    "--", label=melt_label)
+plt.scatter(s.PT['T'], s.PT['P'], marker="*", label="Sample")
 plt.xlabel(r"Temperature [$^\circ$C]")
 plt.ylabel("Pressure [GPa]")
+plt.legend()
 plt.gca().invert_yaxis()
 plt.show()
