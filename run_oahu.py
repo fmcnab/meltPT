cd4a6a1ec24ce8dc30b626c28e98672a5d42f35f
Fergus <fmcnab93@gmail.com>
2022-04-08 15:34:50 +0200

    Add simple conductive geotherm to plot.

--- run_oahu.py
+++ run_oahu.py
@@ -14,13 +14,23 @@ lz = m.lithologies.katz.lherzolite()
 # lz.alphas = 30. # shorttle
 # lz.DeltaS = 407. # shorttle
 mantle = m.mantle([lz], [1], ['Lz'])
-max_P = -lz.parameters['A2'] / (2.*lz.parameters['A3'])
+# max_P = -lz.parameters['A2'] / (2.*lz.parameters['A3'])
+max_P = 7.
 P_sol = np.arange(0., max_P, 0.1)
 T_sol = [lz.TSolidus(P) for P in P_sol]
 
-
 # ---- Fit Tp to suite
 s.find_suite_potential_temperature(mantle, find_bounds=True)
+
+# ---- Conductive geotherm stuff
+density = 3300.
+g = 9.81
+z_lab = 60.e3
+P_lab = z_lab * density * g / 1.e9
+T_lab = mantle.adiabat(P_lab, s.potential_temperature)
+
+# ---- Plot
+plt.plot([0.,T_lab], [0.,P_lab])
 plt.plot(T_sol, P_sol, "k")
 lab=r"$T_p$ = $%i^{+%i}_{-%i}$ $^\circ$C" % (
     s.potential_temperature, 
@@ -32,10 +42,10 @@ plt.plot(s.lower_path.T, s.lower_path.P, ":")
 plt.scatter(s.PT['T'], s.PT['P'], marker="*")
 plt.xlabel(r"Temperature [$^\circ$C]")
 plt.ylabel("Pressure [GPa]")
+plt.xlim((T_sol[0]),T_sol[-1])
 plt.legend()
 plt.gca().invert_yaxis()
 plt.show()
-
 
 # ---- Plot Pressure vs. La/Sm
 
